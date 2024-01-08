1772ed7b7cda7032971ccee0837a22d02d795e9b
Mohamad <m.jehad.kh@gmail.com>
2024-01-08 12:59:08 +0200

    enhancing vew members feature

--- server.py
+++ server.py
@@ -11,11 +11,81 @@ class Member:
     def __init__(self, name, birthdate,height,weight,gender,phone,email):
         self.name = name
         self.gender = gender.lower()
-        self.birthdate = datetime.strptime(birthdate, '%Y-%m-%d').date()  
+        if isinstance(birthdate, str):
+            self.birthdate = datetime.strptime(birthdate, '%Y-%m-%d').date()
+        else:
+            self.birthdate = birthdate  
         self.height=height
         self.weight=weight
         self.phone=phone
         self.email=email
+      # Getter and setter for 'name'
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        self._name = new_name
+
+    # Getter and setter for 'gender'
+    @property
+    def gender(self):
+        return self._gender
+
+    @gender.setter
+    def gender(self, new_gender):
+        self._gender = new_gender.lower()
+
+    # Getter and setter for 'birthdate'
+    @property
+    def birthdate(self):
+        return self._birthdate
+
+    @birthdate.setter
+    def birthdate(self, new_birthdate):
+        if isinstance(new_birthdate, str):
+            self._birthdate = datetime.strptime(new_birthdate, '%Y-%m-%d').date()
+        else:
+            self._birthdate = new_birthdate  
+    # Getter and setter for 'height'
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, new_height):
+        self._height = new_height
+
+    # Getter and setter for 'weight'
+    @property
+    def weight(self):
+        return self._weight
+
+    @weight.setter
+    def weight(self, new_weight):
+        self._weight = new_weight
+
+    # Getter and setter for 'phone'
+    @property
+    def phone(self):
+        return self._phone
+
+    @phone.setter
+    def phone(self, new_phone):
+        self._phone = new_phone
+
+    # Getter and setter for 'email'
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, new_email):
+        self._email = new_email
+
+    # Rest of the class methods
+
     """
     this function used to calculate the age of the member based on his birthdate
     """
@@ -44,24 +114,16 @@ class Member:
 
 def getAllMembersData():
     file = open("MembersData.txt")
-    members=file.read().strip()
+    members = file.read().strip()
     file.close()
-    members=members.split("\n")
-    
-    all_members=[]
+    members = members.split("\n")
+    all_members = []
     for member in members:
-        member=member.split(";")
-        info_dict = {
-        "name": member[0],  
-        "birthdate": member[1],
-        "height": int(member[2]),
-        "weight": int(member[3]),
-        "gender": member[4],
-        "phone": member[5],
-        "email": member[6]  
-        }
-        all_members.append(info_dict)
+        member_data = member.split(";")
+        member_obj = Member(member_data[0],(member_data[1]) , int(member_data[2]), int(member_data[3]), member_data[4], member_data[5], member_data[6])
+        all_members.append(member_obj)
     return all_members
+
 
 
 
@@ -76,13 +138,13 @@ def homepage():
     text=""
     for member in members:
         text+=("<tr>")
-        text+=("<td>"+member["name"]+"</td>")
-        text+=("<td>"+member["birthdate"]+"</td>")
-        text+=("<td>"+str(member["height"])+"</td>")
-        text+=("<td>"+str(member["weight"])+"</td>")
-        text+=("<td>"+member["gender"]+"</td>")
-        text+=("<td>"+member["phone"]+"</td>")
-        text+=("<td>"+member["email"]+"</td>")
+        text+=("<td>"+member.name+"</td>")
+        text+=("<td>"+str(member.calculate_age())+"</td>")
+        text+=("<td>"+str(member.height)+"</td>")
+        text+=("<td>"+str(member.weight)+"</td>")
+        text+=("<td>"+member.gender+"</td>")
+        text+=("<td>"+member.phone+"</td>")
+        text+=("<td>"+member.email+"</td>")
         text+=("</tr>")
     return get_html("index").replace("$$MEMBERS$$",text)
 
