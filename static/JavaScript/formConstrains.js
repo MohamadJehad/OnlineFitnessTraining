7dba10d3decdffae83becf14b545668f895f17bf
Mohamad <m.jehad.kh@gmail.com>
2024-01-30 13:59:51 +0200

    correcting the form validation order and solve problem of 'event' is deprecated

--- static/JavaScript/formConstrains.js
+++ static/JavaScript/formConstrains.js
@@ -28,66 +28,77 @@ import { checkContainsNumbers, checkMinLength } from "./constrains.js";
 //this message is empty by default and will be used to tell the trainser what data is wrong
 const errorMsg=document.getElementById("errorMsg");
 
-document.getElementsByClassName("form")[0].addEventListener("submit",()=>{
+document.getElementsByClassName("form")[0].addEventListener("submit", (event)=>{
 const elementsArray = document.getElementsByClassName('input');
 for(var i=0 ;i<elementsArray.length ; i++){
     let name=elementsArray[i].name;
     let value=elementsArray[i].value;
-    console.log(name+" "+value);
-    
     if(checkEmpty(value,"Empty "+name+": Please Enter "+name)){
         event.preventDefault();
+        break;
     }
     else if(name == 'name' && checkContainsNumbers(value,"member's Name must not contains any numbers")){
-        event.preventDefault(); 
+       event.preventDefault(); 
+        break;
     }
     else if(name == 'name' && checkMinLength(value,3,"Name length must be more than 3")){
         event.preventDefault();
+        break;
     }
     else if (name == 'height' && isNaN(value)) {
         event.preventDefault(); 
-        errorMsg.innerText="height must be a number";      
+        errorMsg.innerText="height must be a number";
+        break;      
     }
     else if(name == 'height' && (Number(value) > 230 || Number(value)< 135)){
         event.preventDefault(); 
-        errorMsg.innerText="height must be bigger than 135 and smaller than 230";       
+        errorMsg.innerText="height must be bigger than 135 and smaller than 230";  
+        break;     
     }
     else if ((name=='phone' || name=='bodyFatPercentage') && isNaN(Number(value)) ) {
         event.preventDefault(); 
         errorMsg.innerText=name+" must be a number";
+        break;
     }
     else if (name=='phone' && (value).length !=11) {
         event.preventDefault(); 
         errorMsg.innerText="Phone must be 11 number";   
+        break;
     }
     else if (name=='weight' && isNaN(Number(value))) {
         event.preventDefault(); 
         errorMsg.innerText="Weight must be a number";
+        break;
     }
     else if(name == 'weight' && (Number(value) > 230 || Number(value)< 30)){
         event.preventDefault(); 
-        errorMsg.innerText="Weight must be bigger than 30 and smaller than 230";       
+        errorMsg.innerText="Weight must be bigger than 30 and smaller than 230";   
+        break;    
     }
     else if(name=='email' && !value.includes("@")){
         event.preventDefault(); 
         errorMsg.innerText="Email must contains @";
+        break;
     }
     else if(name=='email' && value.trim().length <= 8){
         event.preventDefault(); 
         errorMsg.innerText="Email length must be more than 8 ";
+        break;
     }
     else if(name == 'birthdate' && (Number(value.slice(0,4))<1950 || Number(value.slice(0,4))>2020)){
         errorMsg.innerText="Enter valid birthdate"; 
-        console.log("value");
         event.preventDefault();  
+        break;
     }
     else if((name == "disease" || name =="medications" || name=="allergy" || name =="fitnessGoals") && checkMinLength(value,2,"Min length of "+name+" at least 2 char")){
         event.preventDefault();
+        break;
     }
 
     else if( name=='bodyFatPercentage' && (Number(value) > 60 || Number(value)< 5)){
         event.preventDefault(); 
         errorMsg.innerText="vital_fat_prcent must be bigger than 5 and smaller than 60";
+        break;
     }
 }
 })
